a6bc2035c3e73dfd58e9b32d72299960f493a137
calebpotts21 <32202284-calebpotts21@users.noreply.replit.com>
2024-02-19 14:28:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+# Declaration
+#name = "Caleb"
+
+#print(name)
+#print("Hello my name is " + name)
+
+
+#name = input("What is my name? ")
+#print("Hello my name is " + name)
+
+#fahrenheit = input("What is the temperature in Fahrenheit? ")
+#celsius = (float(fahrenheit) - 32) * 5/9
+#print("The temperature in Celsius is " + str(celsius) + "°C")
+#print(f"The temperature in Celsius is {celsius}°C")
+# {} Interpolation
+
+#Find the age of the person if DOB is given
+#dob = input("What is your year of birth? ")
+#age = 2024 - int(dob)
+#print(f"Your age is {age}")
+
+# Area of circle
+#import math as M  
+#radius = input("What is the radius of the circle? ")
+#area = M.pi * float(radius) ** 2
+#print(f"The area of the circle is {area}")
+
+#Input: 70
+#Output: [=======] 70%
+#percentage = input("What percentage has it completed?")
+#completed = float(percentage) // 10
+#loaded = "=" * int(completed)
+#pending = 10-completed
+#pendingRemaining = " " * int(pending)
+#print(f"[{loaded}{pendingRemaining}] {percentage}%")
+
+quote = "I love Python"
+#print(quote[0])
+#print(quote[2])
+
+#print(quote.lower())
+
+#Slicing Operator
+print(quote[2:6])
+print(quote[7:13])
+print(quote[7:])
+print(quote[-1:])
+print(quote[-6:])
+
+#skip  
+print(quote[::2])
+
+#Reverse
+print(quote[::-1])
