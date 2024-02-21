9b7d8101e11a42dfc39ee4b0fcd86ddbb70d525f
Caleb4IR <111176657+Caleb4IR@users.noreply.github.com>
2024-02-21 14:23:03 +0000

    Updating dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,34 +1,67 @@
 # Dictionary -> Hashmap -> Key: Value
 # Key: Unique | Value: Duplicate
 
+# person = {
+#   "name": "Siya Kolisi",
+#   "age": 32,
+#   "country": "South Africa",
+#   "sport": "Rugby"
+# }
+
+# print(person, type(person))
+# print(person["name"])
+# person["age"] += 1
+# print(person)
+
+# #Methods
+# #Iterable -> List, Tuple, dict_keys
+# print(person.keys())
+# print(person.values())
+# print(person.items())
+
+# #loop
+# person = {
+#   "name": "Siya Kolisi",
+#   "age": 32,
+#   "country": "South Africa",
+#   "sport": "Rugby"
+# }
+
+# for detail in person.items():
+#   print(detail[0])
+
+# for key, value in person.items():
+#   print(key,value)
+
+# print(person["height"]) # Error
+# #safety for value when using "get()"
+# print(person.get("height")) #None
+# #get("key", default value)
+# print(person.get("height", 175))
+
+
 person = {
   "name": "Siya Kolisi",
   "age": 32,
-  "country": "South Africa",
+  "address": {
+    "city": "Port Elizabeth",
+    "country": "South Africa"
+  },
+  "school" : "Grey high school",
+  "height": 186,
   "sport": "Rugby"
 }
+print(person["address"]["city"])
+#AttributeError: 'NoneType' object has no attribute 'get'
+#print(person.get("stats").get("points"))
 
-print(person, type(person))
-print(person["name"])
-person["age"] += 1
-print(person)
+print(person.get("stats", {}))
 
-#Methods
-#Iterable -> List, Tuple, dict_keys
-print(person.keys())
-print(person.values())
-print(person.items())
+print(person.get("stats", {}).get("points"))
 
-#loop
-person = {
-  "name": "Siya Kolisi",
-  "age": 32,
-  "country": "South Africa",
-  "sport": "Rugby"
-}
 
-for detail in person.items():
-  print(detail[0])
+#Dictionary comprehension
+nums = {x: x for x in range(10)}
+nums = {x: x**2 for x in range(10)}
+nums = {x: x**2 for x in range(10) if x % 2 == 0}
 
-for key, value in person.items():
-  print(key,value)
--- dictionary_ex.py
+++ dictionary_ex.py
@@ -121,34 +121,145 @@
 
 
 
-guests = [
-  {"name": "Alice", "age": 25, "code": "VIP123"},
-  {"name": "Bob", "age": 17, "code": "VIP123"},
-  {"name": "Charlie", "age": 30, "code": "VIP123"},
-  {"name": "Dave", "age": 22, "code": "GUEST"},
-  {"name": "Eve", "age": 29, "code": "VIP123"}
-]
+# guests = [
+#   {"name": "Alice", "age": 25, "code": "VIP123"},
+#   {"name": "Bob", "age": 17, "code": "VIP123"},
+#   {"name": "Charlie", "age": 30, "code": "VIP123"},
+#   {"name": "Dave", "age": 22, "code": "GUEST"},
+#   {"name": "Eve", "age": 29, "code": "VIP123"}
+# ]
 
-blacklist = ["Dave", "Eve"]
+# blacklist = ["Dave", "Eve"]
 
 
-# Output
-# People who are 21 or above and VIP123
-# Blacklist are not allowed
+# # Output
+# # People who are 21 or above and VIP123
+# # Blacklist are not allowed
 
-# ["Alice", "Charlie"]
-allowed_guest = []
-for guest in guests:
-  if guest["age"] >= 21 and guest["code"] == "VIP123" and guest["name"] not in blacklist:
-    allowed_guest.append(guest["name"])
-print(allowed_guest)
+# # ["Alice", "Charlie"]
+# allowed_guest = []
+# for guest in guests:
+#   if guest["age"] >= 21 and guest["code"] == "VIP123" and guest["name"] not in blacklist:
+#     allowed_guest.append(guest["name"])
+# print(allowed_guest)
 
-#OR
+# #OR
+# #PEP - Pythone Enhancement Proposal 8
 
-allowed_guest = []
-PASS_CODE = "VIP123"
+# allowed_guest = []
+# PASS_CODE = "VIP123"
 
-for guest in guests:
-  if guest["age"] >= 21 and guest["code"] == PASS_CODE and guest["name"] not in blacklist:
-    allowed_guest.append(guest["name"])
-print(allowed_guest)
+# for guest in guests:
+#   if guest["age"] >= 21 and guest["code"] == PASS_CODE and guest["name"] not in blacklist:
+#     allowed_guest.append(guest["name"])
+# print(allowed_guest)
+
+# allowe_guest = [guest["name"] for guest in guests if guest["age"] >= 21 and guest["code"] == PASS_CODE and guest["name"] not in blacklist]
+
+
+# nums = [90, 50, 80]
+
+# for idx in range(len(nums)):
+#   print(idx, nums[idx])
+
+# for num in enumerate(nums):
+#   print(num)
+
+# for idx, num in enumerate(nums):
+#   print(idx, num)
+
+
+# employee = [
+#   {"name": "alex", "experience": 2},
+#   {"name": "Gemma"},
+#   {"name": "Rashay", "age": 4},
+#   {"name": "Thato"},
+# ]
+
+# #2024
+# #Expected Output
+# #Task 1, Task2 - Use get()
+# # [
+# #   {"name": "alex", "experience": 3},
+# #   {"name": "Gemma", "experience": 1},
+# #   {"name": "Rashay", "experience": 5},
+# #   {"name": "Thato", "experience": 1},
+# # ]
+
+
+
+# for employee in employees:
+#   employee["experience"] = employee.get("experience", 0) + 1
+
+# print(employees)
+
+# #Task 3
+# #senior is 5 or more experience
+# #Mid Level is 3-5
+# #Junior level is less than 3
+
+# #Expected Output
+# # [
+# #   {"name": "alex", "experience": 3, "status": "Mid-Level"},
+# #   {"name": "Gemma", "experience": 1, "status": "Junior"},
+# #   {"name": "Rashay", "experience": 5, "status": "senior"},
+# #   {"name": "Thato", "experience": 1, "status": "Junior"},
+# # ]
+
+# for employee in employees:
+#   employee["experience"] = employee.get("experience", 0) + 1
+#   experience = employee["experience"]
+#   if experience >= 5:
+#       employee["status"] = "Senior"
+#   elif 3 <= experience < 5:
+#     employee["status"] = "Mid-Level"
+#   else:
+#     employee["status"] = "Junior"
+
+# print(employees)
+
+
+#Copy
+# movie = {
+#   "name": "Mr Bones",
+#   "year": 2001
+# }
+
+# movie_copy1 = movie.copy()
+
+# #unpacking operator  * -> List | ** -> Dictionary
+# movie_copy2 = {**movie} #Copy
+# print(movie_copy2)
+
+# movie_copy2 = {**movie, "rating": 10} #Copy and add
+# print(movie_copy2)
+
+# movie_copy3 = {**movie, "rating": 10, "year": 2002}
+# print(movie_copy3)
+
+# movie_copy4 = {"rating": 10, "year": 2002, **movie}
+# print(movie_copy4)
+
+# movie = {
+#   "name": "Mr Bones",
+#   "year": 2001
+# }
+
+# detail = {
+#   "actor": "Lean schuster",
+#   "director": "Dzithendo"
+# }
+
+# movie_details = {**movie, **detail} #Preferred way to add dictionaries together
+# print(movie_details)
+
+price = [1000, 1200, 400]
+price_copy = [*price] # Copy 
+
+price_copy1 = [50, 40, *price, 60]
+print(price_copy, price_copy1)
+
+t1 = [80, 90]
+t2 = [50, 60]
+t3 = [*t1, *t2]
+print(t3)
